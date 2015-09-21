93f5090f0969f32615974253587948bcd5399724
Cyrille Favreau <cyrille.favreau@epfl.ch>
2015-09-21 12:58:18 +0200

    Updated licencing

--- rendering_resource_manager_service/config/admin.py
+++ rendering_resource_manager_service/config/admin.py
@@ -2,6 +2,26 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=W0212
 
+# Copyright (c) 2014-2015, Human Brain Project
+#                          Cyrille Favreau <cyrille.favreau@epfl.ch>
+#
+# This file is part of RenderingResourceManager
+# <https://github.com/BlueBrain/RenderingResourceManager>
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 3.0 as published
+# by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# All rights reserved. Do not distribute without further notice.
+
 """
 This modules allows database tables to be access from the django admin interface
 """
